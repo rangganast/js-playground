05a57c600ca02fb7bb433ab3dfca3918aee9ee1c
rangganast <rangganast@gmail.com>
2021-04-06 08:46:06 +0700

    re initial commit

--- nestjs-orm-restful/src/models/events/entity/event.entity.spec.ts
+++
@@ -1,7 +0,0 @@
-import { EventEntity } from './event.entity';
-
-describe('EventEntity', () => {
-  it('should be defined', () => {
-    expect(new EventEntity()).toBeDefined();
-  });
-});
--- nestjs-orm-restful/src/models/organizers/entity/organizer.entity.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Organizer } from './organizer.entity';
-
-describe('Organizer', () => {
-  it('should be defined', () => {
-    expect(new Organizer()).toBeDefined();
-  });
-});
